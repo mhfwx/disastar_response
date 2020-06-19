9de96d016b32c997a1ddde37cbcdd129b42fa546
Haofei Ma <mahaofei001@gmail.com>
2020-06-19 19:26:56 -0400

    finish data process

--- data/process_data.py
+++ data/process_data.py
@@ -1,16 +1,31 @@
 import sys
-
+import pandas as pd
+from sqlalchemy import create_engine
 
 def load_data(messages_filepath, categories_filepath):
-    pass
-
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+    df = pd.merge(messages, categories, left_on='id',right_on='id',how='outer')
+    return df
 
 def clean_data(df):
-    pass
+    categories = df.categories.str.split(';', expand = True)
+    row = categories.loc[0]
+    category_colnames = row.apply(lambda x: x[:-2]).values.tolist()
+    categories.columns = category_colnames
+    categories.related.loc[categories.related == 'related-2'] = 'related-1'
+    for column in categories:
+        categories[column] = categories[column].astype(str).str[-1]
+        categories[column] = pd.to_numeric(categories[column])
+    df.drop('categories', axis = 1, inplace = True)
+    df = pd.concat([df, categories], axis = 1)
+    df.drop_duplicates(subset = 'id', inplace = True)
+    return df
 
 
-def save_data(df, database_filename):
-    pass  
+def save_data(df, database_filepath):
+    engine = create_engine('sqlite:///' + database_filepath)
+    df.to_sql('FigureEight', engine, index=False)  
 
 
 def main():
@@ -27,7 +42,8 @@ def main():
         
         print('Saving data...\n    DATABASE: {}'.format(database_filepath))
         save_data(df, database_filepath)
-        
+
+
         print('Cleaned data saved to database!')
     
     else:
